5a0779a99219ca4042dbbf5dc7d2fd6d7f82c256
Nicolas F <83544753+nicofqcode@users.noreply.github.com>
2022-05-31 16:02:09 -0400

    Second commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Header from './components/Header';
+import Characters from './components/Characters';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Characters />
+      <h1>Hola mudno</h1>
+    </div>
+  );
+}
+
+export default App;
